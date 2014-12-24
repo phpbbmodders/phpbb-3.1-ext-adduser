4ccb152e6d41e9c6501e9da1d6c25a387123912f
Raphaël <Galixte@users.noreply.github.com>
2014-12-24 16:46:53 +0100

    Update for FRENCH translation for acp_adduser.php
    Update for FRENCH translation for acp_adduser.php

--- language/fr/acp_adduser.php
+++ language/fr/acp_adduser.php
@@ -5,7 +5,7 @@
  * @author RMcGirr83  (Rich McGirr) rmcgirr83@rmcgirr83.org
  * @copyright (c) 2014 phpbbmodders.net
  * @license GNU General Public License, version 2 (GPL-2.0)
- * @translated into French by ForumsFaciles (http://www.forumsfaciles.fr)
+ * @translated into French by ForumsFaciles (http://www.forumsfaciles.fr) & Galixte (http://www.galixte.com)
  *
  */
  
@@ -39,22 +39,22 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'ACP_ACCOUNT_ADDED'			=> 'Le compte utilisateur a été créé. L’utilisateur peut désormais se connecter avec le pseudo et le mot de passe envoyés à l’adresse e-mail que vous avez indiquée.',
-	'ACP_ACCOUNT_INACTIVE'		=> 'Le compte utilisateur a été créé. Cependant, les paramètres du forum exigent que l’utilisateur active son compte.<br />A cet effet, une clef d’activation a été envoyée à l’adresse que vous avez indiquée pour l’utilisateur.',
+	'ACP_ACCOUNT_INACTIVE'		=> 'Le compte utilisateur a été créé. Cependant, les paramètres du forum exigent que l’utilisateur active son compte.<br />A cet effet, une clef d’activation a été envoyée à l’adresse email que vous avez indiquée pour l’utilisateur.',
 	'ACP_ACCOUNT_INACTIVE_ADMIN'=> 'Le compte utilisateur a été créé. Cependant, les paramètres du forum exigent que le compte soit activé par un administrateur.<br />Un e-mail a été envoyé aux administrateurs et l’utilisateur sera informé lorsque son compte aura été activé',
 	'ACP_ADD_USER'				=> 'Ajout d’utilisateur via le PCA',
-	'ACP_ADMIN_ACTIVATE'		=> 'Un e-mail sera envoyé à un administrateur pour l’activation du compte. Cependant vous pouvez également cocher la case "activer le compte" ci-dessous pour activer le compe immédiatement une fois qu’il a été créé. L’utilisateur recevra un e-mail contenant les identifiants de connexion.',
+	'ACP_ADMIN_ACTIVATE'		=> 'Un e-mail sera envoyé à un administrateur pour l’activation du compte. Cependant vous pouvez également cocher la case ci-dessous "Activer le compte utilisateur" pour activer le compte immédiatement une fois qu’il a été créé. L’utilisateur recevra un e-mail contenant les identifiants de connexion.',
 	'ACP_EMAIL_ACTIVATE'		=> 'Une fois le compte créé, l’utilisateur recevra un e-mail contenant un lien pour activer son compte.',
 	'ACP_INSTANT_ACTIVATE'		=> 'Le compte sera activé immédiatement. L’utilisateur recevra un e-mail contenant ses identifiants de connexion.',
 
-	'ADD_USER'					=> 'Ajouter l’utilisateur',
-	'ADD_USER_EXPLAIN'			=> 'Créer un nouveau compte utilisateur. Si vos %paramètres d’activation%s sont sur Activation par l’administrateur seulement, vous pourrez activer l’utilisateur immédiatement.',
-	'MOD_VERSION'				=> 'Version %s',
+	'ADD_USER'					=> 'Créer un nouvel utilisateur',
+	'ADD_USER_EXPLAIN'			=> 'Créer un nouveau compte utilisateur. Vous aurez la possibilité d’activer l’utilisateur instantanément, seulement si vos %sparamètres d’activation%s sont configurés sur : “Par l’administrateur”.',
+	'MOD_VERSION'				=> 'version %s',
 	'ADMIN_ACTIVATE'			=> 'Activer le compte utilisateur',
 	'CONFIRM_PASSWORD'			=> 'Confirmer le mot de passe',
-	'EDIT_USER_GROUPS'			=> '%sCliquer ici pour modifier les groupes de cet utilisateur%s',
-	'GROUP_ADD'					=> 'Effectuer une sélection ici ajoutera l’utilisateur au groupe sélectionné ainsi qu’au groupe des utilisateurs enregistrés',
+	'EDIT_USER_GROUPS'			=> '%sCliquer ici pour modifier le(s) groupe(s) de cet utilisateur%s',
+	'GROUP_ADD'					=> 'Ici vous pouvez sélectionner un groupe supplémentaire auquel l’utilisateur sera ajouté, bien qu’il appartiendra au groupe des utilisateurs enregistrés.',
 
 	'CONTINUE_EDIT_USER'		=> '%1$sCliquer ici pour gérer le profil de %2$s%3$s', // e.g.: Click here to edit Joe’s profile.
 	'PASSWORD'					=> 'Mot de passe',
-	'PASSWORD_EXPLAIN'			=> 'Si vous saisissez un mot de passe ici, aucune ne sera généré automatiquement.',
+	'PASSWORD_EXPLAIN'			=> 'La saisie d’un mot de passe ici ne permet pas d’en généré un automatiquement.',
 ));
